0a38923bf32eccdbb2bc3f05f435657ccd9731f3
NareSh-TechHub <nareshbizresearch@gmail.com>
2024-09-21 18:18:54 +0530

    Initial commit for UI

---
+++ UI/BlogApp/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { BlogPostService } from '../services/blog-post.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-blogpost',
+  templateUrl: './add-blogpost.component.html',
+  styleUrls: ['./add-blogpost.component.css']
+})
+export class AddBlogpostComponent {
+  model : AddBlogPost;
+  constructor(private blogpostSerive : BlogPostService,
+    private router : Router){
+    this.model = {
+      title : '',
+      shortDescription : '',
+      featuredImageUrl : '',
+      urlHandle : '',
+      content : '',
+      author : '',
+      publishedDate : new Date(),
+      isVisible : true
+    }
+  }
+
+  onFormSubmit() : void {
+    this.blogpostSerive.createBlogPost(this.model)
+    .subscribe({
+      next : (response) => {
+        this.router.navigateByUrl('/admin/blogposts');
+      }
+    });
+  }
+
+}
